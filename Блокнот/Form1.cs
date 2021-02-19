bebebc52abbcbacf3a0cf09f2c4aff8805a742e2
AlexandrSedelkov <a.sedelkov@list.ru>
2021-02-20 00:00:14 +0300

    Добавьте файлы проекта.

---
+++ Блокнот/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Блокнот
+{
+    public partial class Блокнот : Form
+    {
+        public Блокнот()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            //text
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Это блокнот!", "Блокнот: сведения", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        private void цветToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
